6105ac9c0737ef40b808b96bc516587b79bf21dd
gauravkumarchhipa <gauravchhipa295@gmail.com>
2024-04-30 22:22:45 +0530

    express_json

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ config({
   path: "./config/config.env",
 });
 const app = express();
+app.use(express.json());
 
 import course from "./routers/courseRoutes.js";
 import user from "./routers/userRoutes.js";
@@ -16,10 +17,9 @@ app.use("/api/v1", user);
 export default app;
 app.get("/", (req, res) => {
   res.send(
-    `<h1>Server is working. click <a href=${
-      process.env.NODE_ENV === "development"
-        ? process.env.FRONTEND_URL_LOCAL
-        : process.env.FRONTEND_URL
+    `<h1>Server is working. click <a href=${process.env.NODE_ENV === "development"
+      ? process.env.FRONTEND_URL_LOCAL
+      : process.env.FRONTEND_URL
     }>here</a> to visit frontend.</h1>`
   );
 });
--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,13 +1,12 @@
 import { catchAsyncError } from "../middlewares/catchAsyncError.js";
 import { User } from "../models/User.js";
 import ErrorHandler from "../utils/errorHandler.js";
+
 export const registration = catchAsyncError(async (req, res, next) => {
-  const { name, email, password } = req.body;
-  if (!name || !email || !password)
-    return next(new ErrorHandler("Please enter all field", 400));
-  // const data = "Registration successfully";
+  const { name, email } = req.body;
+  if (!name || !email) return next(new ErrorHandler("Please enter all field", 400));
   const data = await User.find();
-  res.status(200).json({
+  res.status(201).json({
     success: true,
     data,
   });
