33ee32b23d27629c70a4fd3e55a293f5f51f95ef
gaurav.chhipa <gaurav.c@zyapaar.com>
2024-05-01 11:24:03 +0530

    user_registration

--- config/database.js
+++ config/database.js
@@ -2,10 +2,9 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   const { connection } = await mongoose.connect(
-    // process.env.NODE_ENV === "development"
-    //   ? process.env.MONGO_URI_LOCAL
-    //   : 
-    process.env.MONGO_URI
+    process.env.NODE_ENV === "development"
+      ? process.env.MONGO_URI_LOCAL
+      : process.env.MONGO_URI
   );
   console.log(`MongoDB connect with ${connection?.host}`);
 };
--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -6,7 +6,8 @@ import { sendToken } from "../utils/sendToken.js";
 export const registration = catchAsyncError(async (req, res, next) => {
   const { name, email, password } = req.body;
 
-  if (!name && !email && !password) return next(new ErrorHandler("Please enter name, email & password", 400));
+  if (!name && !email && !password)
+    return next(new ErrorHandler("Please enter name, email & password", 400));
   if (!name) return next(new ErrorHandler("Please enter name", 400));
   if (!email) return next(new ErrorHandler("Please enter email", 400));
   if (!password) return next(new ErrorHandler("Please enter password", 400));
@@ -20,19 +21,25 @@ export const registration = catchAsyncError(async (req, res, next) => {
     password,
     avatar: {
       public_id: "temId",
-      url: "tempurl"
-    }
+      url: "tempurl",
+    },
   });
   sendToken(res, user, "Registration Successfully", 201);
 });
 
 export const login = catchAsyncError(async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email && !password)
+    return next(new ErrorHandler("Please enter  email & password", 400));
+  if (!email) return next(new ErrorHandler("Please enter email", 400));
+  if (!password) return next(new ErrorHandler("Please enter password", 400));
   // const data = "login successfully";
-  const data = await User.find();
-  res.status(200).json({
-    success: true,
-    data,
-  });
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) return next(new ErrorHandler("Incorrect Email or Password", 401));
+
+  const isMatch = await User.comparePassword(password);
+  console.log(isMatch);
+  sendToken(res, user, `Welcome back ${user?.name}`, 200);
 });
 
 export const logout = catchAsyncError(async (req, res, next) => {
